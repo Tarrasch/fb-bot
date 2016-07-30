1fb287c811db097628acf92f769220b06e9d736e
Arash Rouhani <miffoljud@gmail.com>
2016-07-30 18:02:27 +0700

    Test messengerbot

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import os
 from pprint import pprint
 import requests
 
+
+
+
 app = Flask(__name__)
 
 ACCESS_TOKEN = os.environ.get('FB_PAGE_ACCESS_TOKEN', 'this_should_be_configured')
@@ -16,6 +19,41 @@ def reply(user_id, msg):
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
     print(resp.content)
 
+
+def reply_test(user_id):
+    from messengerbot import MessengerClient, messages, attachments, templates, elements
+
+    # Manully initialise client
+    messenger = MessengerClient(access_token=ACCESS_TOKEN)
+
+
+    recipient = messages.Recipient(recipient_id=user_id)
+
+    # Send text message
+    message = messages.Message(text='Hello World')
+    request = messages.MessageRequest(recipient, message)
+    messenger.send(request)
+
+    # Send button template
+    web_button = elements.WebUrlButton(
+            title='Show website',
+            url='https://petersapparel.parseapp.com'
+            )
+    postback_button = elements.PostbackButton(
+            title='Start chatting',
+            payload='USER_DEFINED_PAYLOAD'
+            )
+    template = templates.ButtonTemplate(
+            text='What do you want to do next?',
+            buttons=[
+                web_button, postback_button
+                ]
+            )
+    attachment = attachments.TemplateAttachment(template=template)
+
+    message = messages.Message(attachment=attachment)
+    request = messages.MessageRequest(recipient, message)
+    messenger.send(request)
 
 @app.route('/')
 def hello_world():
@@ -38,6 +76,7 @@ def handle_incoming_messages():
         sender = data['entry'][0]['messaging'][0]['sender']['id']
         message = data['entry'][0]['messaging'][0]['message']['text']
         reply(sender, message)
+        reply_test(sender)
     except Exception as ex:
         print(ex)
     return "ok"
