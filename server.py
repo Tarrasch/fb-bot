453f0ab016376a3767cd138959cde085c70909c7
Arash Rouhani <miffoljud@gmail.com>
2016-07-30 15:49:04 +0700

    Update endpoints

--- server.py
+++ server.py
@@ -10,13 +10,18 @@ VERIFY_TOKEN = "my_voice_is_my_password_verify_me"
 def reply(user_id, msg):
     data = {
         "recipient": {"id": user_id},
-        "message": {"text": msg}
+        "message": {"text": "reply: " + msg}
     }
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
     print(resp.content)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
+def hello_world():
+    return 'Hello, World! https://www.facebook.com/Quanbot-1658289714492002/'
+
+
+@app.route('/webhook/', methods=['GET'])
 def handle_verification():
     if request.args['hub.verify_token'] == VERIFY_TOKEN:
         return request.args['hub.challenge']
@@ -24,7 +29,7 @@ def handle_verification():
         return "Invalid verification token"
 
 
-@app.route('/', methods=['POST'])
+@app.route('/webhook/', methods=['POST'])
 def handle_incoming_messages():
     data = request.json
     sender = data['entry'][0]['messaging'][0]['sender']['id']
