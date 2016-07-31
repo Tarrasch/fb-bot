e75d573fbacae19b4f12779ad2ff5a2d6df61db4
Arash Rouhani <miffoljud@gmail.com>
2016-07-31 10:16:31 +0700

    lol

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def getuser(recipient_id):
     parsed_users = []
     for user in all_users:
         try:
-            user.json = json.__file__(user.dict_str)
+            user.json = json.loads(user.dict_str)
             parsed_users.append(user)
         except json.JSONDecodeError:
             pass
