94cf77a7821c59c64244737d0fefae89e2e5aa95
Arash Rouhani <miffoljud@gmail.com>
2016-07-31 08:47:48 +0700

    Rollback postgres

--- app.py
+++ app.py
@@ -1,26 +1,16 @@
 from flask import Flask, request
-from flask.ext.sqlalchemy import SQLAlchemy
+import requests
 import quanbot.handler
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 
 VERIFY_TOKEN = "my_voice_is_my_password_verify_me"
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    dict_str = db.Column(db.String())
+def reply_test(recipient_id):
+    from messengerbot import MessengerClient, messages
+    from messengerbot import attachments, templates, elements
 
-    def __init__(self, dict_str):
-        self.dict_str = dict_str
-
-    def __repr__(self):
-        return getattr(self, 'json', None)
-
-    @property
-    def recipient_id(self):
-        return self.json['recipient_id']
 
 
 @app.route('/')
@@ -43,43 +33,14 @@ def handle_incoming_messages():
         entries = data['entry'][0]['messaging']
         for entry in entries:
             sender = entry['sender']['id']
-
             message = entry.get('message', {}).get('text')
             payload = entry.get('postback', {}).get('payload')
             simplify = message or payload
             if simplify:
-                user = getuser(sender)
-                new_user = quanbot.handler.handler_message(user, simplify)
-                saveuser(new_user)
-
+                quanbot.handler.handler_message(sender, simplify)
     except Exception as ex:
         print(ex)
     return "ok"
-
-
-def getuser(recipient_id):
-    import json
-    all_users = User.query.all()
-    parsed_users = []
-    for user in all_users:
-        try:
-            user.json = json.loads(user.dict_str)
-            parsed_users.append(user)
-        except json.JSONDecodeError:
-            pass
-    for parsed_user in parsed_users:
-        if parsed_user.json['recipient_id'] == recipient_id:
-            return parsed_user
-
-    user = User('TODO')
-    user.json = dict(recipient_id=recipient_id)
-
-
-def saveuser(user):
-    import json
-    user.dict_str = json.dumps(user.json)
-    db.session.add(user)
-    db.session.commit()
 
 
 if __name__ == '__main__':
--- quanbot/handler.py
+++ quanbot/handler.py
@@ -5,32 +5,14 @@ import quanbot.user_state
 ACCESS_TOKEN = os.environ.get('FB_PAGE_ACCESS_TOKEN', 'conf_this')
 
 
-def handler_message(user, message):
+def handler_message(sender, message):
     replies = quanbot.replies.Replies(
             access_token=ACCESS_TOKEN,
-            recipient_id=user.recipient_id,
+            recipient_id=sender,
             )
     user_state = quanbot.user_state.UserState.getIfNew(
-            recipient_id=user.recipient_id,
+            recipient_id=sender,
             replies=replies,
             )
     print(user_state)
     user_state.run_behavior(message)
-
-
-# def get_pq_conn():
-#     import os
-#     import psycopg2
-#     import urlparse
-
-#     urlparse.uses_netloc.append("postgres")
-#     url = urlparse.urlparse(os.environ["DATABASE_URL"])
-
-#     conn = psycopg2.connect(
-#         database=url.path[1:],
-#         user=url.username,
-#         password=url.password,
-#         host=url.hostname,
-#         port=url.port
-#     )
-#     return conn
